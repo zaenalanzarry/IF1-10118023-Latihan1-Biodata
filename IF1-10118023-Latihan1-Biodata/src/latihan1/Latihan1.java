0f0dad4717920c4931f347667ffbba810e2a3736
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2019-09-25 14:18:27 +0700

    Add files via upload

---
+++ IF1-10118023-Latihan1-Biodata/src/latihan1/Latihan1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan1;
+
+/**
+ * @author Zaenal PC
+ * NAMA              : Zaenal Anzarry
+ * KELAS             : IF-01
+ * NIM               : 10118023
+ * Deskripsi Program : Perintah Menampilkan Value Ke Layar 
+ */
+
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------");
+        System.out.println("NAMA\t\t: Zaenal Anzarry");
+        System.out.println("ALAMAT\t\t: Jalan Cimerang RT 02 RW 05 No.33");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 02 Desember 1999");
+        System.out.println("---------------");
+        System.out.println("HOBI\t\t: Membuat Hal Baru dalam Kemajuan Teknologi di Bidang IoT");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Sop Buah");
+    }
+    
+}
